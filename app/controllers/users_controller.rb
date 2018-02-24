8b2a1dff5cbff5a9f4318dcb15cb5c161a0225c9
Thien Le <thienle.luong@gmail.com>
2018-02-24 15:48:54 +0700

    Finish user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,7 +11,8 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
-
+      flash[:success] = "Welcome #{@user.name} to the Sample Apple"
+      redirect_to @user
   	else
   		render 'new'
   end
