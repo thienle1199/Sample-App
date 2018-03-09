f67205c61f7c2100d896a3abfc2884457a21adad
Thien Le <thienle.luong@gmail.com>
2018-03-09 20:25:11 +0700

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -62,18 +62,16 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "Sample_app_#{Rails.env}"
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'sampleapple.herokuapp.com'
-  config.action_mailer.default_url_options = { host: host }
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.gmail.com",
-    :port => 587,
-    :user_name => "thienle1144@gmail.com",
-    :password => "875849Luong",
-    :authentication => :plain,
-    :enable_starttls_auto => true
-}
-  config.action_mailer.perform_caching = false
 
+  config.action_mailer.smtp_settings = {
+      :address => "smtp.gmail.com",
+      :port => 587,
+      :user_name => "thienle1144@gmail.com",
+      :password => "875849Luong",
+      :authentication => :plain,
+      :enable_starttls_auto => true
+  }
+  host = 'sampleapple.herokuapp.com'
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
